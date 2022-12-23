7edc416341efaf8ba1d4179155874e42b4eb7bba
idm1try <dimasavchenko1706@icloud.com>
2022-12-23 15:39:06 +0500

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-/* eslint-disable-next-line @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme')
-
 /* @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
-      },
       animation: {
         in: 'in .6s both',
       },
