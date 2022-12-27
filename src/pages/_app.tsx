83858d53787089148a1d73826d5b9374bcde877e
idm1try <dimasavchenko1706@icloud.com>
2022-12-27 23:28:23 +0500

    feat: vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Meta from '@/components/meta'
 import '@/styles/index.css'
 import { Inter } from '@next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from 'next-themes'
 import { AppProps } from 'next/app'
 
@@ -15,6 +16,7 @@ const Website = ({ Component, pageProps, router }: AppProps) => (
     `}</style>
     <div className='mx-auto max-w-3xl px-6 pb-20 selection:bg-neutral-200 dark:selection:bg-neutral-700'>
       <Meta />
+      <Analytics />
       <main className='pt-16'>
         <Component {...pageProps} key={router.route} />
       </main>
